55ed920ebae58d1d5718ff876a5ebccdbca4a4b6
shpala2km <kuramshinmaksim881@gmail.com>
2026-04-28 19:26:54 +0500

    переделка

--- backend/conway_backend/settings.py
+++ backend/conway_backend/settings.py
@@ -61,14 +61,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'conway_backend.wsgi.application'
 
 # ====================== PostgreSQL ======================
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('DB_NAME'),
+#         'USER': env('DB_USER'),
+#         'PASSWORD': env('DB_PASSWORD'),
+#         'HOST': env('DB_HOST'),
+#         'PORT': env('DB_PORT'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
