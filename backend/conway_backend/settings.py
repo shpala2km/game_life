d963bbecef67bf15ebe95e0ac6752a25b546c639
shpala2km <kuramshinmaksim881@gmail.com>
2026-04-28 14:27:16 +0500

    чиню вход
    1

--- backend/conway_backend/settings.py
+++ backend/conway_backend/settings.py
@@ -11,7 +11,9 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', 'http://81.26.187.199/', '81.26.187.199'
-                                                   'http://81.26.187.199/api/auth/login', 'http://81.26.187.199/api/auth/register'])
+                                                   'http://81.26.187.199/login', 'http://81.26.187.199/register',
+                                                   'http://127.0.0.1:8000/api/auth/login/',
+                                                   'http://127.0.0.1:8000/api/auth/register/'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
